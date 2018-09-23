12bd9db9cf5edc4d7abb567b35ecb2c808b96841
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2018-09-23 22:14:22 +0200

    Add documentation

--- src/AspNetCore.AsyncInitialization/DependencyInjection/AsyncInitializationServiceCollectionExtensions.cs
+++ src/AspNetCore.AsyncInitialization/DependencyInjection/AsyncInitializationServiceCollectionExtensions.cs
@@ -6,14 +6,28 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Provides extension methods to register async initializers.
+    /// </summary>
     public static class AsyncInitializationServiceCollectionExtensions
     {
+        /// <summary>
+        /// Registers necessary services for async initialization support.
+        /// </summary>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the service to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddAsyncInitialization(this IServiceCollection services)
         {
             services.TryAddTransient<AsyncInitializer>();
             return services;
         }
 
+        /// <summary>
+        /// Adds an async initializer of the specified type.
+        /// </summary>
+        /// <typeparam name="TInitializer">The type of the async initializer to add.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the service to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddAsyncInitializer<TInitializer>(this IServiceCollection services)
             where TInitializer : class, IAsyncInitializer
         {
@@ -22,6 +36,13 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddTransient<IAsyncInitializer, TInitializer>();
         }
 
+        /// <summary>
+        /// Adds the specified async initializer instance.
+        /// </summary>
+        /// <typeparam name="TInitializer">The type of the async initializer to add.</typeparam>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the service to.</param>
+        /// <param name="initializer">The service initializer</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddAsyncInitializer<TInitializer>(this IServiceCollection services, TInitializer initializer)
             where TInitializer : class, IAsyncInitializer
         {
@@ -30,6 +51,12 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddSingleton<IAsyncInitializer>(initializer);
         }
 
+        /// <summary>
+        /// Adds an async initializer with a factory specified in <paramref name="implementationFactory" />.
+        /// </summary>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the service to.</param>
+        /// <param name="implementationFactory">The factory that creates the async initializer.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddAsyncInitializer(this IServiceCollection services, Func<IServiceProvider, IAsyncInitializer> implementationFactory)
         {
             return services
@@ -37,6 +64,12 @@ namespace Microsoft.Extensions.DependencyInjection
                 .AddTransient(implementationFactory);
         }
 
+        /// <summary>
+        /// Adds an async initializer whose implementation is the specified delegate.
+        /// </summary>
+        /// <param name="services">The <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" /> to add the service to.</param>
+        /// <param name="initializer">The delegate that performs async initialization.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IServiceCollection AddAsyncInitializer(this IServiceCollection services, Func<Task> initializer)
         {
             return services
--- src/AspNetCore.AsyncInitialization/Hosting/AsyncInitializationWebHostExtensions.cs
+++ src/AspNetCore.AsyncInitialization/Hosting/AsyncInitializationWebHostExtensions.cs
@@ -5,8 +5,16 @@ using Microsoft.Extensions.DependencyInjection;
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Hosting
 {
+    /// <summary>
+    /// Provides extension methods to perform async initialization of an application.
+    /// </summary>
     public static class AsyncInitializationWebHostExtensions
     {
+        /// <summary>
+        /// Initializes the application, by calling all registered async initializers.
+        /// </summary>
+        /// <param name="host">The web host.</param>
+        /// <returns>A task that represents the initialization completion.</returns>
         public static async Task InitAsync(this IWebHost host)
         {
             using (var scope = host.Services.CreateScope())
