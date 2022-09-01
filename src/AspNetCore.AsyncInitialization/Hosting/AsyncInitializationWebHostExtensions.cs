25e296f2f6224b46b7d5bdc2372d676bf90fcb83
Georgy <tarasverq@gmail.com>
2022-09-01 20:21:43 +0300

    1. Added Initialization for IHost

--- src/AspNetCore.AsyncInitialization/Hosting/AsyncInitializationWebHostExtensions.cs
+++ src/AspNetCore.AsyncInitialization/Hosting/AsyncInitializationWebHostExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using AspNetCore.AsyncInitialization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Hosting
@@ -18,16 +19,34 @@ namespace Microsoft.AspNetCore.Hosting
         /// <returns>A task that represents the initialization completion.</returns>
         public static async Task InitAsync(this IWebHost host)
         {
-            using (var scope = host.Services.CreateScope())
+            await Init(host.Services);
+        }
+        
+        /// <summary>
+        /// Initializes the application, by calling all registered async initializers.
+        /// </summary>
+        /// <param name="host">The web host.</param>
+        /// <returns>A task that represents the initialization completion.</returns>
+        public static async Task InitAsync(this IHost host)
+        {
+            await Init(host.Services);
+        }
+
+        private static async Task Init(IServiceProvider sp)
+        {
+            using (var scope = sp.CreateScope())
             {
                 var rootInitializer = scope.ServiceProvider.GetService<RootInitializer>();
                 if (rootInitializer == null)
                 {
-                    throw new InvalidOperationException("The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
+                    throw new InvalidOperationException(
+                        "The async initialization service isn't registered, register it by calling AddAsyncInitialization() on the service collection or by adding an async initializer.");
                 }
 
                 await rootInitializer.InitializeAsync();
             }
         }
+
+        
     }
 }
